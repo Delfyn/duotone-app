cbb544ee2c8daf1c66e283b5f6a3424fe29ca7bb
Florian Nagel <hello@floriannagel.net>
2016-12-24 15:28:57 +0100

    Add Cloudfront distribution for website bucket

--- infrastructure/website.tf
+++ infrastructure/website.tf
@@ -48,3 +48,54 @@ resource "aws_s3_bucket" "duotone_website_redirect" {
     redirect_all_requests_to = "${var.website_bucket_name}"
   }
 }
+
+# AWS Cloudfront for caching
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.duotone_website.bucket}.s3.amazonaws.com"
+    origin_id   = "duotone_website"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Managed by Terraform"
+  default_root_object = "index.html"
+
+  aliases = ["${var.domain_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "duotone_website"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
