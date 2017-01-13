b552c1a4ea9581aa1a8ffefebf83ad469a36e550
Florian Nagel <hello@floriannagel.net>
2017-01-13 21:01:00 +0100

    Add test for ADD_DUOTONE_COLOR reducer

--- src/reducers/__tests__/ui.js
+++ src/reducers/__tests__/ui.js
@@ -1,9 +1,39 @@
-import { TOGGLE_COLOR_PICKER } from '../../constants/actionTypes';
+import { ADD_DUOTONE_COLOR, TOGGLE_COLOR_PICKER } from '../../constants/actionTypes';
 import reducer, { initialState } from '../ui';
 
 describe('errors reducer', () => {
   it('should return the initialState', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should handle ADD_DUOTONE_COLOR', () => {
+    expect(
+      reducer({}, {
+        type: ADD_DUOTONE_COLOR,
+      }),
+    ).toEqual({
+      showColorPicker: false,
+    });
+
+    expect(
+      reducer({
+        showColorPicker: false,
+      }, {
+        type: ADD_DUOTONE_COLOR,
+      }),
+    ).toEqual({
+      showColorPicker: false,
+    });
+
+    expect(
+      reducer({
+        showColorPicker: true,
+      }, {
+        type: ADD_DUOTONE_COLOR,
+      }),
+    ).toEqual({
+      showColorPicker: false,
+    });
   });
 
   it('should handle TOGGLE_COLOR_PICKER', () => {
